f2d1c4c69f618b6d0427d523dafef513c1807b44
Raleigh Landscaping Pros <dev@raleighlandscapingpros.com>
2026-05-15 21:26:17 -0400

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://raleighlandscapingpros.com',
+  integrations: [sitemap()],
   build: {
     format: 'directory',
   },
